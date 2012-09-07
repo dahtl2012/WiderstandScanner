54a4f15febfd8c3d77713e3d93b35b070908dad9
Klaus Bauernfeind <bauernfeind.klaus@gmx.at>
2012-09-07 09:37:34 +0200

    Erstellung von ThirdViewController

---
+++ WiderstandScannerGit/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  WiderstandScannerGit
+//
+//  Created by Klaus Bauernfeind on 07.09.12.
+//  Copyright (c) 2012 HTL Hollabrunn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
---
+++ WiderstandScannerGit/ThirdViewController.m
@@ -0,0 +1,43 @@
+//
+//  ThirdViewController.m
+//  WiderstandScannerGit
+//
+//  Created by Klaus Bauernfeind on 07.09.12.
+//  Copyright (c) 2012 HTL Hollabrunn. All rights reserved.
+//
+
+#import "ThirdViewController.h"
+
+@interface ThirdViewController ()
+
+@end
+
+@implementation ThirdViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
