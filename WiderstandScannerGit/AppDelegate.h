60d9fdaa98636002ea80ab72490415bab1de80e9
Dominik Arnhof <d.arnhof@gmail.com>
2012-09-07 09:10:10 +0200

    Initial Commit

---
+++ WiderstandScannerGit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WiderstandScannerGit
+//
+//  Created by Dominik Arnhof on 07.09.12.
+//  Copyright (c) 2012 HTL Hollabrunn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WiderstandScannerGit/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  WiderstandScannerGit
+//
+//  Created by Dominik Arnhof on 07.09.12.
+//  Copyright (c) 2012 HTL Hollabrunn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
