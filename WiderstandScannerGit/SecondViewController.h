3144db31bcf1a9d6b0b50f37d2aa9571c8036c09
Dominik Arnhof <d.arnhof@gmail.com>
2012-09-11 19:37:53 +0200

    camera view eingebaut und wenn vom gerät unterstützt Autofokus aktivieren.

--- WiderstandScannerGit/SecondViewController.h
+++ WiderstandScannerGit/SecondViewController.h
@@ -7,7 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
 @interface SecondViewController : UIViewController
+
+@property(nonatomic, retain) AVCaptureStillImageOutput *stillImageOutput;
+
+@property (weak, nonatomic) IBOutlet UIView *vImagePreview;
+@property (weak, nonatomic) IBOutlet UIImageView *vImage;
+- (IBAction)captureNow:(id)sender;
 
 @end
--- WiderstandScannerGit/SecondViewController.m
+++ WiderstandScannerGit/SecondViewController.m
@@ -8,11 +8,14 @@
 
 #import "SecondViewController.h"
 
+
 @interface SecondViewController ()
 
 @end
 
 @implementation SecondViewController
+@synthesize vImagePreview;
+@synthesize vImage;
 
 - (void)viewDidLoad
 {
@@ -22,8 +25,50 @@
 
 - (void)viewDidUnload
 {
+    [self setVImagePreview:nil];
+    [self setVImage:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+    
+    AVCaptureSession *session = [[AVCaptureSession alloc] init];
+	session.sessionPreset = AVCaptureSessionPresetMedium;
+    
+	CALayer *viewLayer = self.vImagePreview.layer;
+	NSLog(@"viewLayer = %@", viewLayer);
+    
+	AVCaptureVideoPreviewLayer *captureVideoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:session];
+    
+	captureVideoPreviewLayer.frame = self.vImagePreview.bounds;
+	[self.vImagePreview.layer addSublayer:captureVideoPreviewLayer];
+    
+	AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    
+	NSError *error = nil;
+	AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:device error:&error];
+	if (!input) {
+		// Handle the error appropriately.
+		NSLog(@"ERROR: trying to open camera: %@", error);
+	}
+	[session addInput:input];
+    
+    if ([device isFocusModeSupported:AVCaptureFocusModeContinuousAutoFocus] == YES) {
+        device.focusMode = AVCaptureFocusModeContinuousAutoFocus;
+    } else {
+        NSLog(@"Kein Autofokus!");
+    }
+    
+    
+    
+    self.stillImageOutput = [[AVCaptureStillImageOutput alloc] init];
+    NSDictionary *outputSettings = [[NSDictionary alloc] initWithObjectsAndKeys: AVVideoCodecJPEG, AVVideoCodecKey, nil];
+    [self.stillImageOutput setOutputSettings:outputSettings];
+    
+    [session addOutput:self.stillImageOutput];
+	[session startRunning];
+    
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -34,5 +79,41 @@
         return YES;
     }
 }
+
+- (IBAction)captureNow:(id)sender {
+    
+    AVCaptureConnection *videoConnection = nil;
+	for (AVCaptureConnection *connection in self.stillImageOutput.connections)
+	{
+		for (AVCaptureInputPort *port in [connection inputPorts])
+		{
+			if ([[port mediaType] isEqual:AVMediaTypeVideo] )
+			{
+				videoConnection = connection;
+				break;
+			}
+		}
+		if (videoConnection) { break; }
+	}
+    
+	NSLog(@"about to request a capture from: %@", self.stillImageOutput);
+	[self.stillImageOutput captureStillImageAsynchronouslyFromConnection:videoConnection completionHandler: ^(CMSampleBufferRef imageSampleBuffer, NSError *error)
+     {
+		 /*CFDictionaryRef exifAttachments = CMGetAttachment( imageSampleBuffer, CGImagePropertyExifDictionary, NULL);
+		 if (exifAttachments)
+		 {
+             // Do something with the attachments.
+             NSLog(@"attachements: %@", exifAttachments);
+		 }
+         else
+             NSLog(@"no attachments");*/
+         
+         NSData *imageData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageSampleBuffer];
+         UIImage *image = [[UIImage alloc] initWithData:imageData];
+         
+         self.vImage.image = image;
+	 }];
+}
+
 
 @end
